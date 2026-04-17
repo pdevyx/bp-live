4b8d74383f8c00b2da6b7b8e6fb3557a155c6a57
pdevyx <86372766+pdevyx@users.noreply.github.com>
2026-04-18 00:07:16 +0200

    feat: trip fit to bounds animation

--- src/routes/trip/$id.tsx
+++ src/routes/trip/$id.tsx
@@ -1,64 +1,89 @@
+import { createFileRoute } from "@tanstack/react-router"
+import { $api } from "../__root"
+import { FUTAR_API_VERSION } from "@/lib/constants"
+import { useEffect, useMemo } from "react"
+import { decode, encode } from "@googlemaps/polyline-codec"
+import { MapRoute, useMap } from "@/components/ui/map"
+import VehiclesLayer from "@/features/vehicles/vehicles"
+import TripDetails from "@/features/trips/trip-details"
+import StopsLayer from "@/features/stops/stops"
+import { LngLatBounds } from "maplibre-gl"
 
-import { createFileRoute } from '@tanstack/react-router'
-import { $api } from '../__root';
-import { FUTAR_API_VERSION } from '@/lib/constants';
-import { useMemo } from 'react';
-import { decode, encode } from "@googlemaps/polyline-codec";
-import { MapRoute } from '@/components/ui/map';
-import VehiclesLayer from '@/features/vehicles/vehicles';
-import TripDetails from '@/features/trips/trip-details';
-import StopsLayer from '@/features/stops/stops';
-
-export const Route = createFileRoute('/trip/$id')({
-  component: RouteComponent,
+export const Route = createFileRoute("/trip/$id")({
+    component: RouteComponent,
 })
 
 function RouteComponent() {
-  const { id } = Route.useParams()
+    const { id } = Route.useParams()
 
-  const { data } = $api.useQuery(
-    "get",
-    "/{dialect}/api/where/trip-details",
-    {
-      params: {
-        path: {
-          dialect: "otp"
+    const { map } = useMap()
 
+    const { data } = $api.useQuery(
+        "get",
+        "/{dialect}/api/where/trip-details",
+        {
+            params: {
+                path: {
+                    dialect: "otp",
+                },
+                query: {
+                    appVersion: import.meta.env.VITE_APP_VERSION ?? "1.0.0",
+                    version: FUTAR_API_VERSION,
+                    key: import.meta.env.VITE_FUTAR_API_KEY,
+                    tripId: id,
+                },
+            },
         },
-        query: {
-          appVersion: import.meta.env.VITE_APP_VERSION ?? "1.0.0",
-          version: FUTAR_API_VERSION,
-          key: import.meta.env.VITE_FUTAR_API_KEY,
-          tripId: id
+        {
+            refetchInterval: 10000,
+            placeholderData: (prev) => prev,
         }
-      },
-    },
-    {
-      refetchInterval: 10000,
-      placeholderData: (prev) => prev
-    }
-  );
+    )
 
-  const path: [number, number][] = useMemo(() => {
-    const points = data?.data.entry.polyline?.points
+    const path: [number, number][] = useMemo(() => {
+        const points = data?.data.entry.polyline?.points
 
-    console.log(data)
+        console.log(data)
 
-    if (!points) {
-      return []
-    }
+        if (!points) {
+            return []
+        }
 
-    const decoded = decode(points).map(((l) => l.reverse()))
+        const decoded = decode(points).map((l) => l.reverse())
 
-    return decoded
-  }, [data])
+        return decoded
+    }, [data])
 
+    useEffect(() => {
+        if (!map || !path.length > 0) return
 
+        const bounds = path.reduce((bounds, coord) => {
+            return bounds.extend(coord)
+        }, new LngLatBounds(path[0], path[0]));
 
-  return <>
-    <MapRoute coordinates={path} color={`#${data?.data.entry.vehicle?.style?.icon.color ?? "888"}`} width={4} opacity={0.8} />
-    <VehiclesLayer filter={['==', ['get', 'tripId'], id]} />
-    { data && <TripDetails data={data?.data} /> }
-  </>
+        map.fitBounds(bounds, {
+             padding: {
+                top: 100,
+                bottom: 100,
+                left: 300,
+                right: 50
+             }
+        })
+        
+    }, [map, path])
 
+    return (
+        <>
+            <MapRoute
+                coordinates={path}
+                color={`#${data?.data.entry.vehicle?.style?.icon.color ?? "888"}`}
+                width={4}
+                opacity={0.8}
+                interactive={true}
+            />
+            <VehiclesLayer filter={["==", ["get", "tripId"], id]} />
+            {data && <TripDetails data={data?.data} />}
+        </>
+    )
 }
+
